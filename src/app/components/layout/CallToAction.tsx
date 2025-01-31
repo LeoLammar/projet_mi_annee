b2564c8e03d172366de592d634b83e2932afe892
notxarco <paul-victor.delerue@student.junia.com>
2025-01-31 13:24:01 +0100

    fix image accueil

--- src/app/components/layout/CallToAction.tsx
+++ src/app/components/layout/CallToAction.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // Importation directe de l'image
-//import imageSrc from "../../media/mentorat-calltoaction.png";
+import imageSrc from "./media/mentorat-calltoaction.webp";
 
 const CallToAction: React.FC = () => {
   return (
     <div className="flex flex-col md:flex-row items-center justify-between p-8 bg-white gap-8">
       {/* Left Section: Content */}
       <div className="md:w-1/2 w-full order-2 md:order-1">
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">Join Us !</h1>
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">Rejoignez-nous</h1>
         <p className="text-gray-600 mb-6">
           Découvrez notre programme de mentorat conçu pour vous aider à atteindre vos objectifs professionnels et personnels.
           Rejoignez une communauté passionnée et bénéficiez de conseils personnalisés.
@@ -25,11 +25,9 @@ const CallToAction: React.FC = () => {
 
       {/* Right Section: Image */}
       <div className="md:w-1/2 w-full order-1 md:order-2">
-        {/* <img
-          src={imageSrc}
-          alt="Placeholder"
-          className="w-full h-auto object-cover rounded-lg shadow-md"
-        /> */}
+        { 
+          <img src="/media/mentorat-calltoaction.webp" alt="Illustration du mentorat" className="w-full h-auto object-cover rounded-lg shadow-md" />
+         }
       </div>
     </div>
   );
