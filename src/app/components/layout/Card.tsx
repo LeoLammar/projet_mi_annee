34dc789fffeca0c8d866eabdab581618e85abf7b
notxarco <paul-victor.delerue@student.junia.com>
2025-01-31 13:39:43 +0100

    fix bouton

--- src/app/components/layout/Card.tsx
+++ src/app/components/layout/Card.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 interface CardProps {
   name: string;
@@ -8,38 +9,35 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ name, imageSrc }) => {
   return (
     <div className="max-w-[14rem] bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-[#D9D9D9]">
-      <a href="#">
-        <img 
-          className="rounded-t-lg" 
-          src={imageSrc} 
-          alt={`Photo of ${name}`} 
-        />
-      </a>
+      
+      {/* Image avec gestion d'erreur */}
+      <img 
+        className="rounded-t-lg" 
+        src={imageSrc || "/media/default-image.webp"} 
+        alt={`Photo of ${name}`} 
+      />
+
+      {/* Contenu texte */}
       <div className="p-3 text-center">
-        <a href="#">
-          <h2 className="mb-2 text-lg font-bold tracking-tight" style={{ color: '#3C6E71' }}>
-            {name}
-          </h2>
-        </a>
-        <a 
-          href="#" 
-          className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white rounded-full focus:ring-4 focus:outline-none focus:ring-blue-300"
-          style={{ backgroundColor: '#3C6E71', transition: 'background-color 0.3s, color 0.3s, border-color 0.3s' }}
-        //   onMouseEnter={(e) => {
-        //     (e.target as HTMLElement).style.backgroundColor = 'white';
-        //     (e.target as HTMLElement).style.color = '#3C6E71';
-        //     (e.target as HTMLElement).style.borderColor = '#3C6E71';
-        //     (e.target as HTMLElement).style.borderWidth = '2px';
-        //   }}
-        //   onMouseLeave={(e) => {
-        //     (e.target as HTMLElement).style.backgroundColor = '#3C6E71';
-        //     (e.target as HTMLElement).style.color = 'white';
-        //     (e.target as HTMLElement).style.borderColor = 'transparent';
-        //     (e.target as HTMLElement).style.borderWidth = '0';
-        //   }}
-        >
-          Read more
-        </a>
+        
+        {/* Titre */}
+        <h2 className="mb-2 text-lg font-bold tracking-tight text-[#3C6E71]">
+          {name}
+        </h2>
+
+        {/* Bouton avec effet hover */}
+        <Link to="/login">
+          <button 
+            aria-label={`Voir plus sur ${name}`}
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-white 
+              rounded-full focus:ring-4 focus:outline-none focus:ring-blue-300 
+              bg-[#3C6E71] hover:bg-white hover:text-[#3C6E71] 
+              hover:border-[#3C6E71] hover:border-2 transition-all duration-300"
+          >
+            Voir plus
+          </button>
+        </Link>
+        
       </div>
     </div>
   );
